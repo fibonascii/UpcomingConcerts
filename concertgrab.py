376c18cb29461a45501de4c10d0b0f9b316abcfc
ReaganKirby <reaganjkirby@gmail.com>
2017-04-23 15:42:43 -0500

    Finished formatting event_data

--- concertgrab.py
+++ concertgrab.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas
+import re
+from datetime import datetime
 
 def make_request(url):
     response = requests.get(url)
@@ -21,9 +23,14 @@ def get_table_rows(soup):
     
     for id_number, row in enumerate(rows):
         cols = row.find_all('td')
+        date = cols[1].get_text()
+        remove_characters = re.sub('[a-zA-Z]{0,}', '', str(date))
+        format_date = re.sub(r"-([2017]{0,4})([0-9]{0,1}:)", r"-\1 \2", str(remove_characters))        
+        #dateTime = datetime.strptime(format_date, '%m-%d-%Y %H:%M:%S') 
+         
         concert['id'].append(id_number)  
         concert['name'].append( cols[0].get_text())
-        concert['date'].append( cols[1].get_text())
+        concert['date'].append( format_date)
         concert['venue'].append( cols[2].get_text())
 
     concertTable = pandas.DataFrame( concert )
@@ -31,17 +38,28 @@ def get_table_rows(soup):
     
     return concerts
 
+
 def format_events(concerts):
     events = {
      'summary': [],
      'location': [],
-     }
+     'start': {
+     'datetime': [] },
+     'end': {
+     'datetime': [] },
+          
+    }
 
     for concert in concerts.values():
         events['location'] = concert[2]
         events['summary'] = concert[1]
+        events['start']['datetime'] = concert[0]
+        events['end']['datetime'] = concert[0]
         print(events)
+
+    return events
+
  
 request = make_request('http://concertsdallas.com/')
 rows = get_table_rows(request)
-events = format_events(rows)
+build_formatting = format_events(rows)
